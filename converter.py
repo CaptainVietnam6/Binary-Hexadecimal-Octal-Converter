68786f5cb1b04f1cff736b7ed88d18050334e9c8
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-09-07 03:56:20 -1000

    finished it

--- converter.py
+++ converter.py
@@ -37,11 +37,11 @@ print("[1] hexadecimal (base 16)")
 print("[2] decimal (base 10)\n")
 output_base_choice = input("What base would you like to convert to? ")
 
-if output_base_choice == 0 or output_base_choice == "binary":
+if output_base_choice == "0" or output_base_choice == "binary":
 	output_base_descriptor = "binary"
-elif output_base_choice == 1 or output_base_choice == "hexadecimal":
+elif output_base_choice == "1" or output_base_choice == "hexadecimal":
 	output_base_descriptor = "hexadecimal"
-elif output_base_choice == 2 or output_base_choice == "decimal":
+elif output_base_choice == "2" or output_base_choice == "decimal":
 	output_base_descriptor = "decimal"
 else:
     print("Please enter a valid input!\nExiting in 3 seconds...")
@@ -59,34 +59,98 @@ def rev_string(reverse_input):
         index_num -= 1
     reversed_string = "".join(reversed_string_list)
     
+#CONVERTS USER INPUT INTO ALL UPPPERCASE
+input_value = input_value.upper()
 
 #SCRIPTS TO CALCULATE OUTPUTS
 #converts any inputs into decimal first
-rev_string(input_value)
 
-if output_base_descriptor == "decimal":
-    calc_output = input_value
-elif output_base_descriptor == "binary":
+if input_base_descriptor == "decimal":
+    decimal_output = input_value
+
+elif input_base_descriptor == "binary":
+    rev_string(input_value)
     bi_to_dec_list = []
-    power_two = 1
+    power = 1
     current_digit = 0
     for i in range(len(input_value)): #repeats this for each digit in the input
-        sub_output = int(reversed_string[current_digit]) * power_two
+        sub_output = int(reversed_string[current_digit]) * power
         bi_to_dec_list.append(sub_output)
 
-        power_two *= 2
+        power *= 2
         current_digit += 1
-    test = sum(bi_to_dec_list)
-    print(test)
+    decimal_output = sum(bi_to_dec_list)
+
+elif input_base_descriptor == "hexadecimal":
+    power = len(input_value)
+    decimal_output = 0
+    hex_dec_dict = {
+        "0" : "0",
+        "1" : "1",
+        "2" : "2",
+        "3" : "3",
+        "4" : "4",
+        "5" : "5",
+        "6" : "6",
+        "7" : "7",
+        "8" : "8",
+        "9" : "9",
+        "A" : "10",
+        "B" : "11",
+        "C" : "12",
+        "D" : "13",
+        "E" : "14",
+        "F" : "15"
+    }
+    for i in input_value:
+        power -= 1
+        decimal_output += int(hex_dec_dict[str(i)]) * (16 ** power)
 
 
+#converts decimal into desired output
 
-#DETERMINES WHICH CONVERT METHOD TO USE
+if output_base_descriptor == "decimal":
+    final_output = decimal_output
 
+elif output_base_descriptor == "binary":
+    #doesn't work for some reason
+    '''
+    def dec_to_binary(dec_input):
+        global final_output
+        if dec_input >= 1:
+            dec_to_binary(dec_input // 2)
+        final_output = dec_input % 2
+        return final_output
+    dec_to_binary(decimal_output)
+    '''
+    final_output = int(bin(int(decimal_output))[2:])
 
-'''
-output_value = "***"
-print("\nThe " + input_base_descriptor + " number " + input_value +
-      " is equivalent to the " + output_base_descriptor + " number " +
-      output_value)
-'''
+elif output_base_descriptor == "hexadecimal":
+    power = 0
+    final_output = 0
+    dec_hex_list = {
+        "0" : "0",
+        "1" : "1",
+        "2" : "2",
+        "3" : "3",
+        "4" : "4",
+        "5" : "5",
+        "6" : "6",
+        "7" : "7",
+        "8" : "8",
+        "9" : "9",
+        "10" : "A",
+        "11" : "B",
+        "12" : "C",
+        "13" : "D",
+        "14" : "E",
+        "15" : "F"
+    }
+    #for i in decimal_output:
+        #power += 1
+        #final_output -= str(dec_hex_list[str(i)]) * (16 // power)
+
+    final_output = str(hex(int(decimal_output))[2:])
+
+#DESIRED OUTPUT PRINT
+print(f"The {output_base_descriptor} representation of {input_value} ({input_base_descriptor}) is {final_output}")
